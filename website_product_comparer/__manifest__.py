37cb4965d60e6951bef0084aa98985c97b75b7a2
Arun <kumar.ak.arun3@gmail.com>
2017-08-20 00:44:07 +0530

    Updated manifest file

--- website_product_comparer/__manifest__.py
+++ website_product_comparer/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website_sale'],
 
     # always loaded
     'data': [
