694f1f1e807ef9dc276c8e42a43947defaad29a0
Sushant <sushantdhiman@outlook.com>
2017-08-20 00:07:17 +0530

    Added description and images

--- __manifest__.py
+++ __manifest__.py
@@ -15,6 +15,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
+    'price': 25, 
+    'currency': 'EUR',
     'category': 'Website',
     'version': '0.1',
 
